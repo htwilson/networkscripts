70f64b8b901cfce08265c455d3e074802dc52f87
htwilson <hw.m13@proton.me>
2023-01-13 08:41:03 -0800

    second commit

--- juniperscript.py
+++ juniperscript.py
@@ -1,13 +1,21 @@
+'''
+Python Script for Juniper EX3300 Testing
+Dev: Hugo Wilson 
+1/13/2023
+Ver 1.0
+'''
+
 import time
 import serial
 import sys 
 import os.path 
 
-
-#THIS SCRIPT WILL NOT BE ABLE TO HANDLE PASSWORD RESETS FOR NOW
+'''
+THIS SCRIPT WILL NOT BE ABLE TO HANDLE PASSWORD RESETS FOR NOW
+Commmand line inputs are: script.py COM# LPN###
+'''
 def main():
     #get the  number of arguments input into the cmd line
-    # Commmand line inputs are: script.py COM# LPN###
     numArg = len(sys.argv)
 
     #error check the number of arguments
@@ -18,8 +26,6 @@ def main():
     #error check the arguments to make sure they are valid. 
     connNum = sys.argv[1]
     lpn = sys.argv[2]
-    # print(connNum)
-    # print(lpn)
 
     if connNum[:3] != 'COM'and connNum[3:].isdigit() is False:
         print("Invalid Serial Console ID. Terminating program.")
@@ -42,7 +48,7 @@ def main():
     fileObj = open(lpn + '.txt', 'w')
 
     #https://semfionetworks.com/blog/establish-a-console-connection-within-a-python-script-with-pyserial/
-    #try 2: open the serial connection 
+    #Open the serial connection 
     try:
         serObj = serial.Serial(connNum)
     except:
@@ -59,6 +65,7 @@ def main():
         print("Unable to open serial connection. Terminating program.")
         exit(-1)
 
+    #Run the command helper functions
     LoginRoot(serObj, fileObj)
     ShowVersion(serObj, fileObj)
     ShowConfig(serObj, fileObj)
@@ -72,111 +79,136 @@ def main():
     ReqSysZero(serObj, fileObj)
     LoginRoot(serObj, fileObj)
     ReqPwrOff(serObj, fileObj)
-
-    #read the rest of the output then terminate
-
-
     #Logout(serObj, fileObj)
-    #Close the serial connection 
+    
+    #Listen to the remaining output for termination, no additional command used
+    #Cannot use, device takes about two minutes to shutdown, should keep reading until
+    while True:
+        readdata = serObj.readline().decode('ascii')
+        if len(readdata) !=0:
+            fileObj.write(readdata.strip() + '\n')
+            # print(readdata.strip() + '\n')
+            if "Please press any key to reboot." in readdata:
+                break
+    
+    print ("Script complete. Device safe to unplug Terminating program.")
+
+    #Close the serial connection and file, exit program  
     serObj.close()
-
-    #fileObj.write()
     fileObj.close()
-
-    print ("Script complete. Terminating program.")
     exit(0)
 
-#This code will keep reading until the specified string is read, at which point
-#it will execute the command specified 
+'''
+This function will listen for output from the serial connection until
+a keyword, string is found. Once the string is found, the function will send
+the designated command and exit. Formatted as listen first, then send command
+''' 
 def ReadUntilStringIsFound(string, cmd, serialConn, file):
-    #may have to add a wakeup function 
     while True:
         readdata = serialConn.readline().decode('ascii')
-        file.write(readdata.rstrip() + '\n')
-        
-
         #if the len of the data is 0, that means that there is no more output from console, 
-        # send an enter command to advance the cli 
-        if len(readdata) == 0:
-            #have an error check here for if the code hangs 
-            otherCmd = '\r'
-            serialConn.write(otherCmd.encode("utf-8"))
-            time.sleep(1)
+        # send an enter command to wake up the cli
+        # if len(readdata) == 0:
+        #     wakeupCmd = '\r'
+        #     serialConn.write(wakeupCmd.encode("utf-8"))
+        #     time.sleep(1)
+        # else:
+        #     file.write(readdata.strip() + '\n')
+        #     # print(readdata.strip() + '\n')
+        #     # print(string)
         
+        if len(readdata) != 0:
+            file.write(readdata.strip() + '\n')
+
         #The string indicating the return to the cli input has been found, break 
         if string in readdata:
-            print(readdata.rstrip() + '\n')
-            print(string)
+            # print(readdata.strip() + '\n')
+            # print(string)
             serialConn.write(cmd.encode("utf-8"))
             time.sleep(1)
-            break
+            break 
+    return None
 
+#Logs into the device and enters the CLI 
 def LoginRoot(serialConn, file):    
-    print("Device booting up...")
+    print("Device booting up. This may take a moment..")
     #keep reading from the terminal until there is "login:"
     ReadUntilStringIsFound('login: ', 'root\r', serialConn, file)
     print("Entering default credentials...")
-    print("Entering CLI...")
+    print("Entering CLI. This may take a moment...")
     ReadUntilStringIsFound('root@:RE:0% ', 'cli\r', serialConn, file)
     return None
 
+#Logs the user out of the device, function used for dev debugging only
 # def Logout(serialConn, file):
 #     ReadUntilStringIsFound('root> ', 'exit \r', serialConn, file)
 #     ReadUntilStringIsFound('root@:RE:0% ', 'exit \r', serialConn, file)
 #     return None
 
+#Runs the show version command 
 def ShowVersion(serialConn, file):
     print("Running show version...")
     ReadUntilStringIsFound('root>', 'show version\r', serialConn, file)
     return None
 
+#Runs the show config command 
 def ShowConfig(serialConn, file):
     print("Running show configuration...")
     ReadUntilStringIsFound('root>', 'show configuration | no-more\r', serialConn,file)
     return None 
 
+#Runs the show chassis firmware command 
 def ShowChasFw(serialConn, file):
     print("Running show chassis firmware...")
     ReadUntilStringIsFound('root>', 'show chassis firmware\r', serialConn,file)
     return None
 
+#Runs the show chassis hardware command 
 def ShowChasHw(serialConn, file):
     print("Running show chassis hardware...")
     ReadUntilStringIsFound('root>', 'show chassis hardware\r', serialConn,file)
     return None 
 
+#Runs the show chassis environment command 
 def ShowChasEnv(serialConn, file):
     print("Running show chassis environment...")
     ReadUntilStringIsFound('root>', 'show chassis environment\r', serialConn,file)
     return None
 
+#Runs the show system alarms command 
 def ShowSysAlarms(serialConn, file):
     print("Running show system alarms...")
     ReadUntilStringIsFound('root>', 'show system alarms\r', serialConn,file)
     return None
 
+#Runs the show system storage command 
 def ShowSysStorage(serialConn, file):
     print("Running show system storage...")
     ReadUntilStringIsFound('root>', 'show system storage | no-more\r', serialConn,file)
     return None
 
+#Runs the show system license command 
 #must also add functionality to delete license if neccessary 
 def ShowSysLicense(serialConn, file):
     print("Running show system license...")
     ReadUntilStringIsFound('root>', 'show system license\r', serialConn,file)
     return None
 
+#Runs the show interfaces terse command, ensure that hardware is plugged in prior to running
+#Command may be run faster than the device has time to recogize plugged in devices
 def ShowIntTerse(serialConn, file):
     print("Running show interfaces terse...")
     ReadUntilStringIsFound('root>', 'show interfaces terse | no-more\r', serialConn,file)
     return None
 
+#Runs the request system zeroize command 
 def ReqSysZero(serialConn, file):
     print("Running system reboot...")
     ReadUntilStringIsFound('root>', 'request system zeroize\r', serialConn,file)
     ReadUntilStringIsFound('[yes,no] (no)', 'yes\r', serialConn, file)
     return None
 
+#Runs the request system power-off command
 def ReqPwrOff(serialConn, file):
     print("Running graceful shutdown...")
     ReadUntilStringIsFound('root>', 'request system power-off\r', serialConn,file)
@@ -185,3 +217,6 @@ def ReqPwrOff(serialConn, file):
 
 if __name__ == "__main__":
     main()
+
+
+#ADD AN ERROR CHECK TO TERMINATE THE PROGRAM IF NOTHING IS DETECTED FOR MORE THAN X MINS
